e7006f19a7bddaaa8b5ebeba223134ae6247c015
Astna17 <hei.antsa.17@gmail.com>
2024-05-22 21:08:20 +0300

    chore: change Restauration to Restoration

--- src/main/java/com/gestion/restauration/RestorationApplication.java
+++ src/main/java/com/gestion/restauration/RestorationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurationApplication {
+public class RestorationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestaurationApplication.class, args);
+        SpringApplication.run(RestorationApplication.class, args);
     }
 
 }
