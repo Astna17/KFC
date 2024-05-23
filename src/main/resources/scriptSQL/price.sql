71ca791e6fe1995178ac2ffb59cb440ef6753dbe
Astna17 <hei.antsa.17@gmail.com>
2024-05-23 16:18:14 +0300

    chore: change id_menu to id_ingredient at table price

--- src/main/resources/scriptSQL/price.sql
+++ src/main/resources/scriptSQL/price.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "price" (
    id_price SERIAL PRIMARY KEY,
-   id_menu INT NOT NULL,
+   id_ingredient INT NOT NULL,
    actual_unit_price DOUBLE PRECISION NOT NULL,
    modification_date DATE NOT NULL,
-   FOREIGN KEY (id_menu) REFERENCES "menu"(id_menu)
+   FOREIGN KEY (id_ingredient) REFERENCES "ingredient"(id_ingredient)
 );
