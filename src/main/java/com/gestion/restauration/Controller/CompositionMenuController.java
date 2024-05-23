66073a4aa54eded8d5eb7141ddfe067746367912
Astna17 <hei.antsa.17@gmail.com>
2024-05-23 18:19:17 +0300

    feat: endpoint for composition

---
+++ src/main/java/com/gestion/restauration/Controller/CompositionMenuController.java
@@ -0,0 +1,34 @@
+package com.gestion.restauration.Controller;
+
+import com.gestion.restauration.Entity.CompositionMenu;
+import com.gestion.restauration.Service.CompositionMenuService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CompositionMenuController {
+    private final CompositionMenuService compositionMenuService;
+
+    public CompositionMenuController(CompositionMenuService compositionMenuService) {
+        this.compositionMenuService = compositionMenuService;
+    }
+    @GetMapping("/compositionMenu")
+    public List<CompositionMenu> getAllCompositions() throws SQLException{
+        return compositionMenuService.getAllCompositions();
+    }
+    @GetMapping("/compositionMenu/{idComposition}")
+    public int getCompositionById(@PathVariable int idComposition) throws SQLException {
+        return compositionMenuService.getCompositionMenuById(idComposition);
+    }
+    @PostMapping("/compositionMenu")
+    public CompositionMenu createCompositionMenu(@RequestBody CompositionMenu createComposition) throws SQLException{
+        return compositionMenuService.createCompositionMenu(createComposition);
+    }
+    @PutMapping("/compositionMenu/{idComposition}")
+    public CompositionMenu updateCompositionMenu(@PathVariable int idComposition, @RequestBody CompositionMenu updateComposition) throws SQLException{
+        return compositionMenuService.updateCompositionMenu(idComposition, updateComposition);
+    }
+}
