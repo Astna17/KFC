7189bad389b2d744c99f2f7b63ceda41ccd3fd10
Astna17 <hei.antsa.17@gmail.com>
2024-05-23 16:16:14 +0300

    feat: menu endpoint

---
+++ src/main/java/com/gestion/restauration/Controller/MenuController.java
@@ -0,0 +1,35 @@
+package com.gestion.restauration.Controller;
+
+import com.gestion.restauration.Entity.Menu;
+import com.gestion.restauration.Service.MenuService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MenuController {
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    @GetMapping("/menu")
+    public List<Menu> getAllMenus() throws SQLException{
+        return menuService.getAllMenus();
+    }
+    @GetMapping("/menu/{idMenu}")
+    public int getMenuById(@PathVariable int idMenu) throws SQLException {
+        return menuService.getMenuById(idMenu);
+    }
+    @PostMapping("/menu")
+    public Menu createMenu(Menu createMenu) throws SQLException {
+        return menuService.createMenu(createMenu);
+    }
+    @PutMapping("/menu/{idMenu}")
+    public Menu updateMenu(@PathVariable int idMenu, @RequestBody Menu updateMenu) throws SQLException {
+        return menuService.updateMenu(idMenu, updateMenu);
+    }
+}
